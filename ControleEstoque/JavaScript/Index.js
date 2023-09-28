84e2f5a40799b5bfaf915c28af2c04d0599545f0
00018627 <renylson33@gmail.com>
2023-09-27 22:32:00 -0300

    version 2.0

--- ControleEstoque/JavaScript/Index.js
+++ ControleEstoque/JavaScript/Index.js
@@ -39,6 +39,7 @@ class Produto {
             let td_produto = tr.insertCell();
             let td_local = tr.insertCell();
             let td_quantidade = tr.insertCell();
+            let td_acoes = tr.insertCell();
 
             td_id.innerText = this.arrayProdutos[i].id;
             td_produto.innerText = this.arrayProdutos[i].nomeproduto;
@@ -46,6 +47,13 @@ class Produto {
             td_quantidade.innerText = this.arrayProdutos[i].quantidade;
 
             td_id.classList.add('center')
+
+            let btnDelete = document.createElement('button')
+            td_acoes.appendChild(btnDelete);
+            btnDelete.innerHTML = "EXLUIR";
+            btnDelete.style.backgroundColor = '#1ABF66';
+            btnDelete.style.color = '#ffffff';
+            btnDelete.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id + ")");
 
         }
 
@@ -79,6 +87,18 @@ class Produto {
 
         return true;
 
+    }
+
+    deletar(id) {
+
+        let tbody = document.getElementById('tbody');
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if (this.arrayProdutos[i].id == id) {
+                this.arrayProdutos.splice(i, 1);
+                tbody.deleteRow(i);
+            }
+        }
     }
 }
 
